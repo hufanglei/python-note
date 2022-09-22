26050a9a878ff1576d025ba44b3ed2aa4ffa2341
fanglei <fanglei.hu@jingkeinternet.com>
2022-09-22 08:59:24 +0800

    条件语句

---
+++ day1/单分支.py
@@ -0,0 +1,6 @@
+
+
+salary=8000
+#单分支
+if salary < 10000:
+    print("you make too less money， and ugly")
---
+++ day1/双分支.py
@@ -0,0 +1,51 @@
+# salary = 8000
+# if salary < 10000:
+#     print("你挣太少了，又长的丑")
+# else:
+#     print("恭喜满足了在北京生存的基本收入线")
+
+# age_of_oldboy=48
+# guess=int(input("请输入您的年龄>>："))
+# if guess > age_of_oldboy:
+#     print("猜的太大了，往小里试试....")
+# elif guess < age_of_oldboy:
+#     print("猜的太小了，往大里试试....")
+# else:
+#     print("恭喜你，猜对了")
+#
+
+
+# salary = int(input("Salary:"))
+# if salary > 100000:
+#     print("公司就是我的家")
+# elif salary > 50000:
+#     print("996就像呼吸⼀样⾃然")
+# elif salary > 30000:
+#     print("⽼板说什么都是对的，如果有⼈错了，那⼀定是我")
+# elif salary > 20000:
+#     print("⽼板说啥就是啥吧，给钱就⾏")
+# elif salary > 10000:
+#     print("⽼板说的有点问题，但是我不说话")
+# elif salary > 5000:
+#     print("⽼板脑⼦有坑，背后说坏话")
+# elif salary > 2000:
+#     print("⽼板，wqnmlgbxxxx")
+# else:
+#     print("⽼板，我是你爹...")
+
+
+#作业
+score = int(input("Score:"))
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80 and score <= 89:
+    print("B")
+elif score >= 60 and score <= 79:
+    print("C")
+elif score >= 40 and score <= 59:
+    print("D")
+else:
+    print("E")
+
+
+
