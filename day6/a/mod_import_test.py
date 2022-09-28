82a433c8cffd8d69ed98a8b20b2b17901f4be48a
fanglei <fanglei.hu@jingkeinternet.com>
2022-09-28 23:10:32 +0800

    模块跨目录导入

---
+++ day6/a/mod_import_test.py
@@ -0,0 +1,13 @@
+﻿import sys
+import os
+
+d6path = 'E:\\ideawork\\pythonproject\\py8day_camp\\day6'
+print(__file__)  # 打印当前脚本文件路径
+print(os.path.dirname(__file__))  # 打印当前脚本文件路径
+base_path = os.path.dirname(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(__file__)))  # 打印当前脚本文件路径
+# print(sys.path)
+# sys.path.append(d6path)
+# print(sys.path)
+sys.path.append(base_path)
+import my_first_mod
---
+++ day6/模块导入.py
@@ -0,0 +1,22 @@
+﻿# -*- coding:utf-8 -*-
+
+# import os
+# os.
+
+# from os import system
+# system("df -h")
+
+# from djingo.contrib.auth import  authenticate as auth
+# def authenticate()
+# from os from *
+# os.system("df -h")
+
+# 不推荐
+# def chmod():
+# from os import  *
+# chmod()
+
+
+
+import  my_first_mod   #python my_first_mod.py
+my_first_mod.sayHi()
