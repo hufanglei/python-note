507f6030c9823f1737112a6c1b1c958a70b1240e
hfl <3453008585@qq.com>
2023-08-12 18:01:41 +0800

    测试

---
+++ own/demo1/Demo2.py
@@ -0,0 +1,7 @@
+﻿##!/usr/bin/python3
+#
+# input("\n\n按下 enter 键后退出。")
+
+## !/usr/bin/python3
+
+import sys;x = 'runoob';sys.stdout.write(x + '\n')
---
+++ own/demo1/support.py
@@ -0,0 +1,10 @@
+﻿##!/usr/bin/python3
+# Filename: support.py
+import requests
+
+url = "http://123.129.175.157:9000/msj-customer-bucket/2023-08-03/test1.txt"
+
+response = requests.get(url)
+content = response.content.decode("utf-8")
+
+print(content)
